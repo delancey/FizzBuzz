e5e7e488bb0408cd0dac323cf0a023c9be42fcce
Dana <dsdelancey@gmail.com>
2020-06-01 09:43:46 -0400

    updated conditional to numbers divisible b 3, print Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
     }
 
     public String processNumber(int number){
-        if (number == 3){
+        if (number % 3 == 0){
             return "Fizz";
         } else if (number == 5){
             return "Buzz";
