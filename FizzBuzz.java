a74494441ae84c8141d9515524173ee8193b5480
Dana <dsdelancey@gmail.com>
2020-06-01 09:34:08 -0400

    Created second test

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,10 @@
 public class FizzBuzz {
     public void execute(){
+        
+    }
 
+    public String processNumber(int number){
+        return "1";
     }
     
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,8 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class FizzBuzzTest {
     @Test
-    public void test() {
+    public void test(){
 
+    }
+
+    @Test
+    public void shouldProcessInput() {
+        FizzBuzz fizzBuzz = new FizzBuzz();        
+        assertEquals("1", fizzBuzz.processNumber(1));
     }
 }
